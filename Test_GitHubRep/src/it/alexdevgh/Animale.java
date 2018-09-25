30cb83b4faae48bcbcf0ba0292e62ca083d0a619
Alessandro Altemani <alessandro.altemani@carrerajeans.it>
2018-09-25 14:13:50 +0200

    Aggiunti getter e setter

--- Test_GitHubRep/src/it/alexdevgh/Animale.java
+++ Test_GitHubRep/src/it/alexdevgh/Animale.java
@@ -8,4 +8,26 @@ public class Animale {
 	public void saluta() {
 		System.out.println("Ciao");
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Boolean getIsMammifero() {
+		return isMammifero;
+	}
+	
+	
 }
